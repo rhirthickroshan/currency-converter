980f6c4c8a5058d54bd208d73225f0636630c028
Hirthick Roshan <rhirthickroshan@gmail.com>
2026-07-05 22:08:42 +0530

    checking commmit

--- src/main/java/com/currency/security/CustomerUserDetailsService.java
+++ src/main/java/com/currency/security/CustomerUserDetailsService.java
@@ -7,19 +7,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 public class CustomerUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomerUserDetailsService(UserRepository userRepository) {
+    private final Logger logger;
+
+    public CustomerUserDetailsService(UserRepository userRepository, Logger logger) {
         this.userRepository = userRepository;
+        this.logger = logger;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
 
+        logger.log(Level.INFO,"For dummy commit");
         User user = userRepository.findByEmailIgnoreCase(email)
                 .orElseThrow(() ->
                         new UsernameNotFoundException(
