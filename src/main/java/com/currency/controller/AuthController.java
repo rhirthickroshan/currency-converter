422d09463da94365b2e695359805ac0b6e787365
Hirthick Roshan <rhirthickroshan@gmail.com>
2026-07-08 02:39:49 +0530

    dummy check

--- src/main/java/com/currency/controller/AuthController.java
+++ src/main/java/com/currency/controller/AuthController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
+
     private final AuthService authService;
 
     @PostMapping("/register")
